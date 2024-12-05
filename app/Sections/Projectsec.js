ca2e0893d19cb7879d9446404e3f6151c479fc80
Clint George <clintbgeorge@gmail.com>
2024-12-05 22:10:35 +0530

    Added Projects section
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Sections/Projectsec.js
+++ app/Sections/Projectsec.js
@@ -3,47 +3,83 @@ import React from "react";
 const Projectsec = () => {
   return (
     <>
-      <div className="flex flex-row">
+      <div className="flex flex-row mt-28">
         <div className="flex-1">
-          <p className="section2txt">02-PROJECTS</p>
+          <p className="font-kanit font-semibold text-[120px] text-[#949494] rotate-[270deg] mt-32 ml-[-270px] tracking-wider">
+            02-PROJECTS
+          </p>
         </div>
-        <div className="projecttbl">
-          <div className="projectbg">
-            {/* <img className="projimg" src="Images/acadsite"> */}
-            <div className="projdsn"></div>
-            <p className="projdate">2023 - 2024</p>
-            <p className="projtitle">
+        <div className="flex-[4] flex-wrap flex-row flex">
+          <div className="h-[450px] w-[420px] border m-6">
+            <img
+              className="h-[200px] w-[400px] ml-2 mt-2 mb-2"
+              src="/acadsite"
+            />
+            <div className="h-[3px] w-[35px] bg-[#D1D1D1] mt-6 ml-[22px]"></div>
+            <p className="text-[13px] font-kanit font-bold mt-1 ml-[22px]">
+              2023 - 2024
+            </p>
+            <p className="font-kanit text-[27px] font-normal text-[#A9A9A9] mt-1 mx-5">
               ACADEMIC CALENDER WITH INTEGRATED CHATBOT
             </p>
-            <button className="projbtn">CHECK PROJECT</button>
+            <button className="border mt-6 ml-5 px-5 pt-2 pb-[11px] text-[14px]">
+              CHECK PROJECT
+            </button>
           </div>
-          <div className="projectbg">
-            {/* <img className="projimg" src="Images/fcritproj"> */}
-            <div className="projdsn"></div>
-            <p className="projdate">2023 - 2024</p>
-            <p className="projtitle">FCRIT FEE MANAGEMENT SYSTEM</p>
-            <button className="projbtn">CHECK PROJECT</button>
-          </div>
-          <div className="projectbg1">
-            {/* <img className="projimg" src="Images/fcritproj"> */}
-            <div className="projdsn"></div>
-            <p className="projdate">2023 - 2024</p>
-            <p className="projtitle">FCRIT FEE MANAGEMENT SYSTEM</p>
-            <button className="projbtn">CHECK PROJECT</button>
-          </div>
-          <div className="projectbg1">
-            {/* <img className="projimg" src="Images/acadsite"> */}
-            <div className="projdsn"></div>
-            <p className="projdate">2023 - 2024</p>
-            <p className="projtitle">
+          <div className="h-[450px] w-[420px] border m-6">
+            <img
+              className="h-[200px] w-[400px] ml-2 mt-2 mb-2"
+              src="/acadsite"
+            />
+            <div className="h-[3px] w-[35px] bg-[#D1D1D1] mt-6 ml-[22px]"></div>
+            <p className="text-[13px] font-kanit font-bold mt-1 ml-[22px]">
+              2023 - 2024
+            </p>
+            <p className="font-kanit text-[27px] font-normal text-[#A9A9A9] mt-1 mx-5">
               ACADEMIC CALENDER WITH INTEGRATED CHATBOT
             </p>
-            <button className="projbtn">CHECK PROJECT</button>
+            <button className="border mt-6 ml-5 px-5 pt-2 pb-[11px] text-[14px]">
+              CHECK PROJECT
+            </button>
+          </div>
+          <div className="h-[450px] w-[420px] border m-6">
+            <img
+              className="h-[200px] w-[400px] ml-2 mt-2 mb-2"
+              src="/acadsite"
+            />
+            <div className="h-[3px] w-[35px] bg-[#D1D1D1] mt-6 ml-[22px]"></div>
+            <p className="text-[13px] font-kanit font-bold mt-1 ml-[22px]">
+              2023 - 2024
+            </p>
+            <p className="font-kanit text-[27px] font-normal text-[#A9A9A9] mt-1 mx-5">
+              ACADEMIC CALENDER WITH INTEGRATED CHATBOT
+            </p>
+            <button className="border mt-6 ml-5 px-5 pt-2 pb-[11px] text-[14px]">
+              CHECK PROJECT
+            </button>
+          </div>
+          <div className="h-[450px] w-[420px] border m-6">
+            <img
+              className="h-[200px] w-[400px] ml-2 mt-2 mb-2"
+              src="/acadsite"
+            />
+            <div className="h-[3px] w-[35px] bg-[#D1D1D1] mt-6 ml-[22px]"></div>
+            <p className="text-[13px] font-kanit font-bold mt-1 ml-[22px]">
+              2023 - 2024
+            </p>
+            <p className="font-kanit text-[27px] font-normal text-[#A9A9A9] mt-1 mx-5">
+              ACADEMIC CALENDER WITH INTEGRATED CHATBOT
+            </p>
+            <button className="border mt-6 ml-5 px-5 pt-2 pb-[11px] text-[14px]">
+              CHECK PROJECT
+            </button>
           </div>
         </div>
       </div>
 
-      <button className="projexpbtn">EXPLORE OTHER PROJECTS</button>
+      <button className="border mt-16 ml-[49vw] px-6 pt-3 pb-3 text-[13px]">
+        EXPLORE OTHER PROJECTS
+      </button>
     </>
   );
 };
