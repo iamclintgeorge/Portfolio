174218f6f88a321c93747fe250210ae192bdee75
Clint George <clintbgeorge@gmail.com>
2024-12-07 20:51:46 +0530

    Added Footer
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -3,28 +3,29 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div class="footer">
-        <div class="footerleft">
-          <p>Clint George</p>
-          <p>
-            +91 7506950281 &nbsp; | &nbsp; <span>clintbgeorge@gmail.com</span>
+      <div className="flex flex-row mx-[150px] mb-[70px] justify-between mt-10">
+        <div className="text-[18px] font-kanit leading-loose">
+          <p className="text-[21px]">Clint George</p>
+          <p className="text-[#6B6B6B]">
+            +91 7506950281 &nbsp; | &nbsp;{" "}
+            <span className="text-white">clintbgeorge@gmail.com</span>
           </p>
-          <p>&#169; Clint George 2024</p>
+          <p className="text-[#6B6B6B]">&#169; Clint George 2024</p>
         </div>
-        <div class="footerright">
-          <ul class="footerbutton">
+        <div className="text-[18px] font-kanit">
+          <ul className="flex flex-col leading-loose text-right">
             <li>
-              <a class="footlink" href="home.html">
+              <a className="footlink" href="home.html">
                 H O M E
               </a>
             </li>
             <li>
-              <a class="footlink" href="home.html">
+              <a className="footlink" href="home.html">
                 A B O U T &nbsp; M E
               </a>
             </li>
             <li>
-              <a class="footlink" href="home.html">
+              <a className="footlink" href="home.html">
                 C O N T A C T
               </a>
             </li>
