67f2e61b542fda32a394dc6d6798b701f8441372
Clint George <clintbgeorge@gmail.com>
2024-12-09 16:49:24 +0530

    Made header resoponsive for mobile
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="flex flex-row justify-between">
+      <div className="flex w-full m-0 p-0 flex-col sm:flex-row justify-between">
         <div className="flex-1">
-          <p className="text-[2.2vw] font-serif mt-10 ml-56 cursor-pointer text-white">
+          <p className="text-[7vw] ml-1 text-center sm:ml-56text-[2.2vw] font-serif mt-10 cursor-pointer text-white">
             clintgeorge.
           </p>
         </div>
-        <div className="flex-1">
+        <div className="hidden sm:block flex-1">
           <ul className="flex justify-evenly font-montserrat text-[1.2vw] mt-14 text-white mb-10">
             <li>H O M E</li>
             <li>A B O U T &nbsp; M E</li>
