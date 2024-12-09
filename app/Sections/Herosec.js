5d00c2dbef1a8653a89948530969e0a2db2ca517
Clint George <clintbgeorge@gmail.com>
2024-12-09 17:20:56 +0530

    Made herosec resoponsive for mobile
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Sections/Herosec.js
+++ app/Sections/Herosec.js
@@ -3,22 +3,24 @@ import React from "react";
 const Herosec = () => {
   return (
     <>
-      <div className="flex flex-row">
+      <div className="flex flex-col sm:flex-row">
         <div>
           <img
-            className="w-[85vw] mt-12 ml-24 mr-0 grayscale"
+            className="w-[70vw] ml-16 sm:w-[85vw] mt-12 sm:ml-24 mr-0 grayscale"
             src="/Clintphoto.jpeg"
             alt="Profile Picture"
           />
         </div>
-        <div className="mt-28 ml-48">
-          <p className="text-[62px] font-kanit mr-10 mb-16 leading-[60px]">
+        <div className="mt-20 ml-14 mr-10 sm:mt-28 sm:ml-48">
+          <p className="text-[27px] text-center sm:text-[62px] sm:text-left font-kanit mr-10 mb-16 sm:leading-[60px]">
             Crafting custom software solutions that meet your unique needs.
           </p>
-          <button className=" border py-3 px-7">GET IN TOUCH</button>
-          <div className="mt-32 mb-8 h-5 w-56 bg-slate-200 ml-auto"></div>
+          <button className="border ml-20 py-3 px-4 sm:px-7 sm:ml-0">
+            GET IN TOUCH
+          </button>
         </div>
       </div>
+      <div className="mt-24 sm:mt-32 mb-8 h-5 w-56 bg-slate-200 ml-auto"></div>
     </>
   );
 };
