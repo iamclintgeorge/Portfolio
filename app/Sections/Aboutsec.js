e0329bf9cd20e458f5317c68533b9121007674c8
Clint George <clintbgeorge@gmail.com>
2024-12-09 19:12:21 +0530

    Made about section responsive
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Sections/Aboutsec.js
+++ app/Sections/Aboutsec.js
@@ -7,12 +7,12 @@ const Aboutsec = () => {
         <p className="relative text-[22.5vw] font-kanit text-[#191919]">
           About Me
         </p>
-        <div className="absolute flex flex-row mt-[-200px]">
-          <div className="flex-1 text-xl ml-28 font-kanit text-[40px] mr-0 font-semibold">
+        <div className="absolute flex flex-col ml-10 sm:flex-row mt-[-20vw] sm:ml-0 sm:mt-[-200px]">
+          <div className="flex-1 text-xl text-nowrap sm:ml-28 font-kanit text-[40px] mr-0 font-semibold ">
             <p>01-Introduction</p>
           </div>
-          <div className="flex-[3] mr-24 text-[20px] font-kanit text-justify">
-            <p>
+          <div className="flex-[3] mt-12 sm:mt-0 mr-10 sm:mr-24 text-xl sm:text-[20px] font-kanit text-justify">
+            <p className="hidden sm:block">
               I am a passionate and skilled full-stack developer with experience
               in building websites, mobile applications, and desktop software.
               Over the years, I’ve had the opportunity to work with a variety of
@@ -26,10 +26,18 @@ const Aboutsec = () => {
               digital products from scratch, I am committed to delivering
               high-quality software that drives business growth and success.
             </p>
+            <p className="block sm:hidden">
+              I am a passionate full-stack developer skilled in creating
+              websites, mobile apps, and desktop software. With expertise in
+              front-end and back-end development, I build scalable,
+              user-friendly solutions that solve real-world challenges. I thrive
+              on collaborating with teams to deliver high-quality software that
+              meets client needs, drives growth, and delivers long-term value.
+            </p>
           </div>
         </div>
       </div>
-      <div className="mt-40 h-5 w-56 bg-slate-200"></div>
+      <div className="mt-96 sm:mt-40 h-5 w-56 bg-slate-200"></div>
     </>
   );
 };
